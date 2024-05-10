dcd0be9912c310126e643c0d723b9f84e90dea8d
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-10 10:05:01 -0700

    file updated

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -2,7 +2,9 @@
 """ Views module
 """
 from flask import Blueprint
-from api.v1.views.index import *
+from api.v1.views.index import status
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+app_views.route('/status', methods=['GET'], strict_slashes=False)(status)
